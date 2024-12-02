d4054e0fcbe9cbb38a5ef3589433c05470d000c8
Phanthuanthanh/Phanthuanthanh_ENGKS24B_Project-Flappy-bird <Phanthuanthanh270606@gmail.com>
2024-12-02 23:10:22 +0700

    Up load Bai tap Session 10_Lesson 09

---
+++ Session 10_Lesson 09.cpp
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Moi ban nhap so dong va so cot: ");
+    scanf("%d", &n);
+
+    int arr[n][n];
+
+    printf("Moi ban nhap cac phan tu cua mang:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("[%d][%d]: ", i, j);
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    printf("\nMang ban dau:\n"); 
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    int diagonal[n];
+    for (int i = 0; i < n; i++) {
+        diagonal[i] = arr[i][i]; 
+    }
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (diagonal[j] > diagonal[j + 1]) {
+                int temp = diagonal[j];
+                diagonal[j] = diagonal[j + 1];
+                diagonal[j + 1] = temp;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        arr[i][i] = diagonal[i];
+    }
+
+    printf("\nMang sau khi sap xep duong cheo chinh:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+
